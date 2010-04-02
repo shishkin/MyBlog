a74da20aed1e2c398d78b01d2813bcb74d0ded72
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-04-03 01:49:47 +0200

    common assembly info

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("MyBlog")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sergey Shishkin")]
+[assembly: AssemblyProduct("MyBlog")]
+[assembly: AssemblyCopyright("Sergey Shishkin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
