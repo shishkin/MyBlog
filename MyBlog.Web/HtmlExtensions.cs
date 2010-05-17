b1ce5bc5edda46d2d6d62e1c6979a31698fb8c70
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-05-17 21:31:55 +0200

    added html header link extension

---
+++ MyBlog.Web/HtmlExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.HtmlControls;
+
+namespace MyBlog.Web
+{
+    public static class HtmlExtensions
+    {
+        public static HtmlHead AddLink(this HtmlHead head, string href, string rel, string type)
+        {
+            var link = new HtmlLink
+            {
+                Href = href
+            };
+            link.Attributes["rel"] = rel;
+            link.Attributes["type"] = type;
+            head.Controls.Add(link);
+            return head;
+        }
+    }
+}
