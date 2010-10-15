6e464129466545abdd063c450bff1827e3b4d28d
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-10-15 14:51:48 +0200

    implementing mongo-based storage

--- MyBlog.Web/Data/MongoDB.cs
+++ MyBlog.Web/Data/MongoDB.cs
@@ -5,30 +5,63 @@ using System.Linq;
 using System.Web;
 using System.ServiceModel.Syndication;
 
+using Norm;
+using Norm.Configuration;
+using Norm.Collections;
+
 namespace MyBlog.Web.Data
 {
     using Models;
 
     public class MongoDB : IDataStore
     {
+        private const string connectionString = "mongodb://localhost/Blog";
+
+        public MongoDB()
+        {
+            MongoConfiguration.Initialize(config =>
+            {
+                config.For<SyndicationItem>(map =>
+                {
+                    map.IdIs(x => x.Id);
+                    map.ForProperty(x => x.Content).UseAlias("Content");
+                });
+            });
+        }
+
         public void Put(SyndicationItem value)
         {
-            throw new NotImplementedException();
+            InMongo(x => x.Save(value));
         }
 
         public SyndicationItem Get(string id)
         {
-            throw new NotImplementedException();
+            return InMongo(x => x.FindOne(new { Id = id }));
         }
 
         public void Delete(string id)
         {
-            throw new NotImplementedException();
+            InMongo(x => x.Delete(new { Id = id }));
         }
 
         public IEnumerable<SyndicationItem> List()
         {
-            throw new NotImplementedException();
+            return InMongo(x => x.Find());
+        }
+
+        private T InMongo<T>(Func<IMongoCollection<SyndicationItem>, T> func)
+        {
+            T value = default(T);
+            InMongo(x => { value = func(x); });
+            return value;
+        }
+
+        private void InMongo(Action<IMongoCollection<SyndicationItem>> func)
+        {
+            using (var db = Mongo.Create(connectionString))
+            {
+                func(db.Database.GetCollection<SyndicationItem>("Articles"));
+            }
         }
     }
 }
