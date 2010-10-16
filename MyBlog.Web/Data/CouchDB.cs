b47dfdd9d2d9c54e4558c4d2700a52eb66398608
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-10-16 15:07:58 +0200

    clean up for CouchDB

---
+++ MyBlog.Web/Data/CouchDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Syndication;
+
+namespace MyBlog.Web.Data
+{
+    using Models;
+
+    public class CouchDB : IDataStore
+    {
+        public CouchDB()
+        {
+        }
+
+        public void Put(SyndicationItem value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SyndicationItem Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SyndicationItem> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
